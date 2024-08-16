2651040631a316cd081734e9ceacf67cb69bded7
Dmitryser1 <dmitryserafimovich@gmail.com>
2024-08-16 18:59:56 +0400

    no syntax errors

---
+++ src/columns/columns.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ColumnsService } from './columns.service';
+import { ColumnsController } from './columns.controller';
+import { ColumnEntity } from './entities/column.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([ColumnEntity])],
+    controllers: [ColumnsController],
+    providers: [ColumnsService],
+    exports: [ColumnsService],
+})
+export class ColumnsModule {}
